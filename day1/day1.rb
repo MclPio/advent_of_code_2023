4f89356bd179c6a035f3dc878c8615a69b5ca34f
MclPio <113801014+MclPio@users.noreply.github.com>
2023-12-03 20:07:50 -0500

    Add todo for bug for day1

--- day1/day1.rb
+++ day1/day1.rb
@@ -2,61 +2,74 @@ file = File.open('input.txt')
 
 file_data = file.readlines.map(&:chomp)
 
-numbers_dict = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, 
-                seven: 7, eight: 8, nine: 9 }
-
-digit_sum = 0
-
 def first_word(string)
-  numbers_dict = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, 
-                seven: 7, eight: 8, nine: 9 }
-  first_word = []
+  numbers_dict = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
+  first_word = {}
+
   numbers_dict.each do |k, v|
     key = k.to_s
     value = v.to_s
-    # work on searching for key and value at the same time and comparing which one's 
-    # index starts first so that this function outputs the first occurance as a digit to be added to the last part
-    if !string.index(key).nil?
-      first_word << string.index(key)
-      first_word << key
-      return first_word
+
+    first_spelled_digit_idx = string.index(key)
+    first_digit_idx = string.index(value)
+
+    if first_spelled_digit_idx.nil?
+      if first_digit_idx.nil?
+        next
+      else
+        first_word[first_digit_idx] = value
+      end
+    else
+      first_word[first_spelled_digit_idx] = value
     end
   end
-  return nil
+
+  if first_word.empty?
+    nil
+  else
+    first_word.min[1]
+  end
 end
 
 def last_word(string)
-  numbers_dict = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, 
-                seven: 7, eight: 8, nine: 9 }
-  last_word = []
+  numbers_dict = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
+  last_word = {}
+
   numbers_dict.each do |k, v|
     key = k.to_s
+    value = v.to_s
 
-    if !string.rindex(key).nil?
-      last_word << string.rindex(key)
-      last_word << key
-      return last_word
+    first_spelled_digit_idx = string.index(key)
+    first_digit_idx = string.index(value)
+
+    if first_spelled_digit_idx.nil?
+      if first_digit_idx.nil?
+        next
+      else
+        last_word[first_digit_idx] = value
+      end
+    else
+      last_word[first_spelled_digit_idx] = value
+      #logic missing version where both digit and word are not nil
     end
   end
-  return nil
-end
-
-file_data.each do |str|
-  first_digit_idx = str.index(/\d/)
-  last_digit_idx = str.rindex(/\d/)
-
-  print first_word(str)
-  print last_word(str)
-  if first_digit_idx.nil? || last_digit_idx.nil?
-    next
+  puts last_word
+  if last_word.empty?
+    nil
+  else
+    last_word.max[1]
   end
-
-  first_digit = str[first_digit_idx]
-  last_digit = str[last_digit_idx]
-
-  digit_string = first_digit + last_digit
-  digit_sum += digit_string.to_i
 end
 
-puts digit_sum
-puts numbers_dict
+def calibration_sum(data)
+  number_sum = 0
+  data.each do |str|
+    if str.include?('oneight')
+      number_sum += (first_word(str) + last_word(str)).to_i
+      puts("#{first_word(str)} + #{last_word(str)} = #{number_sum} --> #{str}")
+    end
+  end
+  number_sum
+end
+
+puts calibration_sum(file_data)
